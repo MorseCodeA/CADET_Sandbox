823f426a15b2ecf6f6714c122640ee59954e2361
ahoang10 <ahoang10@jhu.edu>
2017-10-18 16:53:51 -0400

    separate dashboard into partials, create topic and instructor template

--- cadetapp/dashboard/urls.py
+++ cadetapp/dashboard/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('r^topic-distribution', views.topic_distribution,
+        name='topic_distribution'),
+    url('r^instructor-distribution/$', views.instructor_distribution,
+        name='instructor_distribution'),
 ]
--- cadetapp/dashboard/views.py
+++ cadetapp/dashboard/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render, get_object_or_404
 def index(request):
     return render(request, 'dashboard/index.html')
 
+def topic_distribution(request):
+    return render(request, 'dashboard/topic_distribution.html')
+
+def instructor_distribution(request):
+    return render(request, 'dashboard/instructor_distribution.html')
+
