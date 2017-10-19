65ea6e2ff9138b99bdd6007d968be05cdc469459
ahoang10 <ahoang10@jhu.edu>
2017-10-18 22:02:27 -0400

    set up more templates, add template dir in settings, set up code for refactor

--- cadetapp/dashboard/urls.py
+++ cadetapp/dashboard/urls.py
@@ -1,10 +1,22 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'dashboard'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url('r^topic-distribution', views.topic_distribution,
-        name='topic_distribution'),
-    url('r^instructor-distribution/$', views.instructor_distribution,
-        name='instructor_distribution'),
+    url('topic-distribution', views.topic_distribution,
+        name='topic-distribution'),
+    url('instructor-distribution', views.instructor_distribution,
+        name='instructor-distribution'),
 ]
+
+# refactor later in classes when have models
+# urlpatterns = [
+#     url(r'^$', views.DashboardView.as_view(), name='index'),
+#     url('topic-distribution', views.TopicDistributionView.as_view(),
+#         name='topic-distribution'),
+#     url('instructor-distribution',
+#         views.InstructorDistributionView.as_view(),
+#         name='instructor-distribution'),
+#     ]
--- cadetapp/dashboard/views.py
+++ cadetapp/dashboard/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render, get_object_or_404
-# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.views import generic
 
 
 def index(request):
+
+    # later will call dashboard or chart instance from Ashley's models,
+    # which contains comments and topics
+
+    # main dashboard located at http://example/dashboard
     return render(request, 'dashboard/index.html')
 
 def topic_distribution(request):
-    return render(request, 'dashboard/topic_distribution.html')
+
+    # http://example/dashboard/topic-distribution
+    return render(request, 'dashboard/topic-distribution.html')
 
 def instructor_distribution(request):
-    return render(request, 'dashboard/instructor_distribution.html')
 
+    # http://example/dashboard/instructor-distribution
+    return render(request, 'dashboard/instructor-distribution.html')
+
+
+
+# for refactoring later, when models can be instantiated for query_set
+
+# class DashboardView(generic.ListView):
+#     context_object_name = 'dashboard'
+#     template_name = 'dashboard/index.html'
+#
+#
+# class TopicDistributionView(generic.DetailView):
+#     template_name = 'dashboard/topic-distribution.html'
+#
+#
+# class InstructorDistributionView(generic.DetailView):
+#     template_name = 'dashboard/instructor-distribution.html'
