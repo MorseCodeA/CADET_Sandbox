3a7423e95f173af74fa7d2b0ec41e18a62551d0e
amorse13 <amorse13@jhu.edu>
2017-10-20 00:55:38 -0400

    Add models in Model.py in dashboard app
    To connect with the back-end, created course, comment,
    instructor, result, and stopword models.

--- cadetapp/dashboard/models.py
+++ cadetapp/dashboard/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# classes below created from back-end-database.rst (D81)
+# I believe we need this to store the information sent up through the back-end
+class Comment(models.Model):
+	primary_key = models.IntegerField()
+	anon_user_id = models.IntegerField()
+	text = models.TextField()
+	course_id = models.IntegerField()
+	instructor_id = models.IntegerField()
+	timestamp = models.DateTimeField('date published')
+
+class Course(models.Model):
+	primary_key = models.IntegerField()
+	department = models.CharField(max_length=50)
+	program = models.CharField(max_length=50)
+	name = models.CharField(max_length=50)
+	section = models.FloatField('format xxx.xxx')
+	year = models.PositiveSmallIntegerField('format xxxx')
+	semester = models.PositiveSmallIntegerField('[1,2,3]')
+
+class Instructor(models.Model):
+	primary_key = models.IntegerField()
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+
+# might need for graphing?
+class Result(models.Model):
+	primary_key = models.IntegerField()
+	query = models.TextField()
+	result = models.TextField()
+	timestamp = models.DateTimeField()
+
+class Stopword(models.Model):
+	primary_key = models.IntegerField()
+	text = models.TextField()
