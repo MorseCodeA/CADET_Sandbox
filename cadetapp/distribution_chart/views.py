63d572cd24bccc35361e5d16475f6a01ed82611b
ahoang10 <ahoang10@jhu.edu>
2017-11-25 14:37:10 -0500

    replace hardcoded instructor chart with ajax call

--- cadetapp/distribution_chart/views.py
+++ cadetapp/distribution_chart/views.py
@@ -21,6 +21,8 @@ class ChartTopicData(APIView):
     def get(self, request, format=None):
         # this is the structure of my expected nested json result object
         # currently it is hard coded, but later I expect to grab Results_Topic instance 
+        # data = {}
+        # data["topic"] = []
         data = {
           "topic": [
             {
@@ -80,42 +82,91 @@ class ChartTopicData(APIView):
 class ChartTopicWordData(APIView):
     # function return mapping of topic and words associated with that topic
     def get(self, request, format=None):
+        # getting last five topic obj
+        # topic_obj = Topic.objects.all().order_by('-id')[:5]
+        # topic_word_data {}
+        # topic_word_data["topic"] = {}
+        # iterate and push the topic data to the same formal as bellow
         topic_word_data = {
-            "topic_id": ["happy, good, fun, yay, smile"],
-            "topic_id": ["sad, boo, mean, hard, lethal"],
-            "topic_id": ["happy, good, fun, yay, smile"],
-            "topic_id": ["happy, good, fun, yay, smile"],
-            "topic_id": ["happy, good, fun, yay, smile"],
+          "topic": [
+            {
+              "topic_id": "1",
+              "words": "happy, good, fun, yay, smile"
+            },
+            {
+              "topic_id": "2",
+              "words": "sad, boo, mean, hard, lethal"
+            },
+            {
+              "topic_id": "3",
+              "words": "hub, hubba, buh, duh, huh"
+            },
+            {
+              "topic_id": "4",
+              "words": "foo, baz, exe, why, zee"
+            },
+            {
+              "topic_id": "5",
+              "words": "red, white, blue, yellow, green"
+            }
+          ]
         }
         return Response(topic_word_data)
 
 class ChartInstructorData(APIView):
     def get(self, request, format=None):
-        instructor_data = {"Instructor Full Name 1": {
-                    "positive": "15",
-                    "neutral": "2",
-                    "negative": "12",
-                },
-                "Instructor Full Name 2": {
-                    "positive": "9",
-                    "neutral": "2",
-                    "negative": "37",
-                },
-                "Instructor Full Name 3": {
-                    "positive": "8",
-                    "neutral": "2",
-                    "negative": "32",
-                },
-                "Instructor Full Name 4": {
-                    "positive": "5",
-                    "neutral": "8",
-                    "negative": "20",
-                },
-                "Instructor Full Name 5": {
-                    "positive": "5",
-                    "neutral": "29",
-                    "negative": "2",
-                },
+        instructor_data = {
+          "instructor": [
+            {
+              "instructor_name": "joe smoe",
+              "sentiment_count": [
+                {
+                  "pos": "28",
+                  "neu": "8",
+                  "neg": "2"
+                }
+              ]
+            },
+            {
+              "instructor_name": "nathalie fozz",
+              "sentiment_count": [
+                {
+                  "pos": "15",
+                  "neu": "3",
+                  "neg": "7"
+                }
+              ]
+            },
+            {
+              "instructor_name": "beavis butthead",
+              "sentiment_count": [
+                {
+                  "pos": "38",
+                  "neu": "8",
+                  "neg": "2"
+                }
+              ]
+            },
+            {
+              "instructor_name": "boaty boatface",
+              "sentiment_count": [
+                {
+                  "pos": "19",
+                  "neu": "2",
+                  "neg": "8"
+                }
+              ]
+            },
+            {
+              "instructor_name": "numbly doo",
+              "sentiment_count": [
+                {
+                  "pos": "7",
+                  "neu": "8",
+                  "neg": "20"
+                }
+              ]
+            }
+          ]
         }
         return Response(instructor_data)
-
