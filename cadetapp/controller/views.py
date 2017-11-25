359558ab2a963c1d62eb703ca8787d1589be0488
amorse13 <amorse13@jhu.edu>
2017-11-25 18:51:16 -0500

    Add funcitons to handle topic chart
    Modified controller/views.py to have functions that take in a
    list of objects and ourputs then in a list of results that can
    be used for chartjs.

--- cadetapp/controller/views.py
+++ cadetapp/controller/views.py
@@ -4,12 +4,66 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
-from dashboard.models import Comment, Instructor, Course, Topic
+from dashboard.models import Comment, Instructor, Course, Topic, Results_Topic
 
 # The purpose of this file is to take Josh's query from the data layer and 
 # organize the information woth the structure from /dashboard/models.py. 
 # This will then be called by Anh's code to display.
-#def organizeResults(result):
+
+# I am assuming that I am recieving an array of topic objects and an 
+# array of comment objects. A topic object has the word and topic_ID, and
+# a comment object will have the tone, which I can use to count the pos,
+# neu, and neg values.
+def organizeTopicCommentResults(comment_list):
+	topic_id_list = []
+	# find out how many topic_ids there are
+	for comment in comment_list:
+		topic_id_list.append(comment.topic_id)
+	
+	# this list has sorted unique values of topic ids (ex: [1,2,3,4,5])
+	topic_id_list = sorted(list(set(topic_id_list)))
+	num_topics = len(topic_id_list)
+
+	# create the tone lists and initlaize it to zero
+	pos_list = [0] * num_topics
+	neu_list = [0] * num_topics
+	neg_list = [0] * num_topics	
+
+	for topic_id in topic_id_list:
+		for comment in comment_list:
+			if (comment.topic_id == topic_id):
+				if (comment.tone == 'pos'):
+					++pos_list[topic_id];
+				elif (comment.tone == 'neu'):
+					++neu_list[topic_id]
+				elif (comment.tone == 'neg'):
+					++neg_list[topic_id]
+
+	result = [topic_id_list, pos_list, neu_list, neg_list]
+	return result
+
+def organizeTopicWordResults(topic_list):
+	topic_id_list = []
+	# find out how many topic_ids there are
+	for topic in topic_list:
+		topic_id_list.append(topic.topic_id)
+	
+	# this list has sorted unique values of topic ids (ex: [1,2,3,4,5])
+	topic_id_list = sorted(list(set(topic_id_list)))
+	num_topics = len(topic_id_list)
+
+	# create a list of lists that map to the topic_id
+	topic_word_list = [] * num_topics
+	
+	for topic_id in topic_id_list:
+		for topic in topic_list:
+			if (topic.topic_id == topic_id):
+				topic_word_list[topic_id].append(topic.text)
+
+	result = [topic_id_list, topic_word_list]
+	return result
+		
+
     # parse json
 #    parsed_json = json.loads(result.GET)
     
