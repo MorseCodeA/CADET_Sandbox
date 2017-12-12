8efc7134d2379eb0bc6cef6ae27eeabe43b3b8ae
Nick Grubaugh <ngrubau2@jhu.edu>
2017-12-11 17:49:02 -0800

    - Added new documentation pages to following python files

--- cadetapp/documentation/urls.py
+++ cadetapp/documentation/urls.py
@@ -3,4 +3,17 @@ from .views import DocumentationView
 
 urlpatterns = [
     url('/', DocumentationView.home, name="doc-home.html"),
+    url('/', DocumentationView.back_end_database, name="back-end-database.html"),
+    url('/', DocumentationView.cadet_tutorial, name="cadet-tutorial.html"),
+    url('/', DocumentationView.data_analysis, name="data-analysis.html"),
+    url('/', DocumentationView.DisplayTopicCommentsFromDataLayer, name="DisplayTopicCommentsFromDataLayer.html"),
+    url('/', DocumentationView.enforce_code_standards, name="enforce-code-standards.html"),
+    url('/', DocumentationView.enforce_test_before_commit, name="enforce-test-before-commit.html"),
+    url('/', DocumentationView.file_uploader, name="file_uploader.html"),
+    url('/', DocumentationView.instructor_topic_dashboard, name="instructor-topic-dashboard.html"),
+    url('/', DocumentationView.metadata_retrieval, name="metadata-retrieval.html"),
+    url('/', DocumentationView.presentation_layer_interface_for_data_results, name="presentation-layer-interface-for-data-results.html"),
+    url('/', DocumentationView.refactor_front_end_code, name="refactor_front_end_code.html"),
+    url('/', DocumentationView.web_dev_docs, name="web-dev-docs.html"),
+    url('/', DocumentationView.web_tutorial, name="web-tutorial.html"),
 ]
