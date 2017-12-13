8b2c9bdf30c9325883f7a9b8d68490f81a96cf0e
jackjzhan <jackzhan@buffalo.edu>
2017-12-12 22:07:18 -0500

    comment fro Jack's code

--- cadetapp/fileupload/forms.py
+++ cadetapp/fileupload/forms.py
@@ -2,12 +2,18 @@ from django import forms
 from .models import Document, Json_Model
 
 class DocumentForm(forms.ModelForm):
+    """
+    Purpose: Form referencing the file being uploaded
+    """
+
     class Meta:
         model = Document
         fields = ('file',)
 
 class JsonForm(forms.ModelForm):
-
+    """
+    Purpose: Specifies the 4 form fields in the models.py
+    """
     class Meta:
         model = Json_Model
         fields = ['files','comments', 'topics', 'iterations']
--- cadetapp/fileupload/views.py
+++ cadetapp/fileupload/views.py
@@ -13,6 +13,12 @@ from .PushJSON import DataPush
 import os
 
 def option_view(request):
+    """
+    Purpose: This function is the main view for the upload options
+    page. The page objects are generated with the JsonForm. The
+    form inputs are pulled in from the Django models and used to
+    create the JSON transport file
+    """
     if request.method == 'POST':
         form = JsonForm(request.POST)
         if form.is_valid():
@@ -38,7 +44,11 @@ def option_view(request):
     return render(request, 'options.html', {'form': form})
 
 def upload_view(request):
-    media_path = settings.MEDIA_ROOT + '/downloads/'
+    """
+    Purpose: This function is the main view for the file upload
+    page. Uses the the default Django file upload documentation
+    with a model and form
+    """
     if request.method == 'POST':
         files = request.FILES.getlist('file')
         form = DocumentForm(request.POST, request.FILES)
@@ -51,7 +61,11 @@ def upload_view(request):
     return render(request, 'file_upload.html', {'form': form})
 
 def upload_progress(request):
-    #Uses Ajax calls to return the upload progress and total length values
+    """
+    Purpose: This function uses Ajax calls to return the upload progress
+    and total length values for the progress bar
+    Works in conjunction with upload_progress$ url.
+    """
     if 'X-Progress-ID' in request.GET:
         progress_id = request.GET['X-Progress-ID']
     elif 'X-Progress-ID' in request.META:
