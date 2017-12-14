4623f54dbda5fa240e0f280c2d7589e88cd88457
LoweReception <jlowe18@jhu.edu>
2017-12-14 09:45:12 -0500

    line wrap updates

--- cadetapp/fileupload/models.py
+++ cadetapp/fileupload/models.py
@@ -7,9 +7,13 @@ class Document(models.Model):
     # model is not returning title of doc in view
     def __unicode__(self):
          return self.name
-
+# END class Document
+     
 class Json_Model(models.Model):
-    files = models.FilePathField(path=settings.MEDIA_ROOT + '/downloads/', match=".*\.csv$")
+    files = models.FilePathField(
+        path=settings.MEDIA_ROOT + '/downloads/',
+        match=".*\.csv$")
     comments = models.PositiveIntegerField()
     topics = models.PositiveIntegerField()
     iterations = models.PositiveIntegerField()
+# END class Json_Model
