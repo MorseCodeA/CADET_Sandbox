39b9304d9bffd67843782e7e7e3ecbf23e579958
LoweReception <jlowe18@jhu.edu>
2017-12-14 09:46:50 -0500

    Fix line wrap

--- cadetapp/fileupload/DataConversion.py
+++ cadetapp/fileupload/DataConversion.py
@@ -1,6 +1,4 @@
-import csv
-import json
-import os
+import csv, json, os
 
 #----------------------------------------------------------------------
 
@@ -9,7 +7,15 @@ class CSVfiletoJSONobj:
 	def _init_(self):
 		self.inputpath = 'string'
 		self.outputpath = 'string'
-		self.fieldnames = ("anon_id","program","modality","course_num_sect_id","instructor_last_name", "instructor_first__name", "course_comments","instructor_comments","additional_comments")
+		self.fieldnames = ("anon_id",
+                                   "program",
+                                   "modality",
+                                   "course_num_sect_id",
+                                   "instructor_last_name",
+                                   "instructor_first__name",
+                                   "course_comments",
+                                   "instructor_comments",
+                                   "additional_comments")
 
 	def get_input_path(self):
 		return self.inputpath
@@ -29,7 +35,6 @@ class CSVfiletoJSONobj:
 	def set_fieldnames(self, fields):
 		self.fieldnames.append(fields)
 		
-
 	def CSVtoJSON_Obj(self):
 		csvfile = open(self.inputpath, 'r')
 		jsonfile = open(self.outputpath, 'w')
@@ -40,5 +45,5 @@ class CSVfiletoJSONobj:
 		for row in reader:
 			json_list.append(row)
 		json.dump(json_list, jsonfile)
-
+# END class CSVfiletoJSONobj
 
