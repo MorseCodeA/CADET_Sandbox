e9d8ff9f43ef40dbca0f2282edffd98b851bd34c
LoweReception <jlowe18@jhu.edu>
2017-12-14 10:24:06 -0500

    Should have been added to the last commit also

--- cadetapp/results/views.py
+++ cadetapp/results/views.py
@@ -1,16 +1,18 @@
 from django.conf import settings
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Results_Set
+from dashboard.views import DashboardView
 import json, polling, random as rng, requests
 
-#-----------------------------------------------------------------------------
-# Purpose:     Create views fuction to retrieve results from data layer and
-#              store it using Results_Set model from /models.py
-#
-# Functions:   retrieve
-#
-#-----------------------------------------------------------------------------
+""" results.view documentation
+Purpose: Create views fuction to retrieve results from data layer and store it
+    using Results_Set model from /models.py
+
+Functions:   
+    retrieve(request,result_id): gather results pertaining to resutls_id from
+        backend database
+"""
 
 def retrieve(request,result_id):
     """
@@ -25,30 +27,38 @@ def retrieve(request,result_id):
     
     Template expected can be found in Results-example.json
     """
-    url = settings.GLOBAL_SETTINGS['BACKEND_URL']+'/api/json/get/ceeCPfmcoi'
-    # Alternative site for example JSONs
-    #url = settings.GLOBAL_SETTINGS['BACKEND_URL']+'users/%s/' % result_id
-    
-    response = 'URL: ' +url+' | Resp OK = '
+    USE_ALTERNATIVE_URL=False    # Takes precident
+    USE_PLACEHOLDER_URL=True
+    if (USE_ALTERNATIVE_URL):
+        url = settings.GLOBAL_SETTINGS['ALTERNATIVE_URL']
+    elif (USE_PLACEHOLDER_URL):
+        url = settings.GLOBAL_SETTINGS['PLACEHOLDER_URL'] + '%s/' % result_id
+    else:
+        url = settings.GLOBAL_SETTINGS['BACKEND_URL'] + '%s/' % result_id    
+
+    response = 'URL: ' +url+' | Resp = '
     
     try:
         # poll the results table and get the JSON object when complete
         polling.poll(lambda: requests.get(url).status_code == 200,
-                     step=1,
-                     max_tries=5)
+                     step=settings.POLLING_SETTINGS['TIMEOUT'],
+                     max_tries=settings.POLLING_SETTINGS['RETRIES'])
         jsonObj = requests.get(url).json()
 
         # create a new result set to store the data
         resultset = Results_Set()
-        resultset.jsonObjs = jsonObj
-        resultset.id = 1
 
-        ALTERNATIVE_URL=False
-        if (ALTERNATIVE_URL):
+        # if useing the alternative url, save it up
+        if (USE_ALTERNATIVE_URL):
+            resultset.id = 1
+            resultset.jsonObj = jsonObj[0]
+
+        # if using the placeholder url, shove in the data
+        elif USE_PLACEHOLDER_URL:
             resultset.id = jsonObj['id'];
             fakedResult={'result_id':jsonObj['id'],
                          'meta_file_info':{},
-                         'results':{'topic_stats':[],'instructor_stats':[]}
+                         'results':{'topics_stats':[],'instructor_stats':[]}
             }
             # Fake Metadata
             fakedMeta={'document_id_number':rng.randint(1,1000),
@@ -56,6 +66,7 @@ def retrieve(request,result_id):
                        'user_selected_number_interations':rng.randint(1,30),
                        'user_selected_number_words_per_topic':rng.randint(1,5)}
             fakedResult['meta_file_info']=fakedMeta
+            
             #Fake Topic Results
             fakedTopic_comments={
                 'positive':jsonObj['company']['name'].split(),
@@ -64,7 +75,8 @@ def retrieve(request,result_id):
             fakedTopic_stats={'words':jsonObj['website'].split(),
                               'comments':fakedTopic_comments}
             fakedTopicStatsList=[fakedTopic_stats,fakedTopic_stats]
-            fakedResult['results']['topic_stats']=fakedTopicStatsList
+            fakedResult['results']['topics_stats']=fakedTopicStatsList
+            
             #Fake Instructor Results
             fakedInstr_comments={
                 'positive':jsonObj['address']['street'].split(),
@@ -77,13 +89,21 @@ def retrieve(request,result_id):
                 'comments':fakedInstr_comments}
             fakedInstrList=[fakedInstr_stats,fakedInstr_stats]
             fakedResult['results']['instructor_stats']=fakedInstrList
-            resultset.jsonObjs = fakedResult
-            
+            resultset.jsonObj = fakedResult
+        #END elif (USE_PLACEHOLDER_URL)
+        
+        else:
+            resultset.id = result_id
+            resultset.jsonObj = jsonObj
+        
         resultset.save()
         response = response + 'OK'
     except polling.MaxCallException:
         response = response + 'FAIL'      
-    return HttpResponse(response)
-    
+    #return HttpResponse(response)
+    return redirect('/dashboard/topic-distribution')
+
 def index(request):
     return HttpResponse("Hello, world. You're at the results index.")
+
+
